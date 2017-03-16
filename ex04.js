7e98624f173af66c1154409f9fd027bdf4603ad1
Amaurea <aurore.chatagnier@gmail.com>
2017-03-16 15:52:12 +0100

    list all the natural numbers below 1000 that are multiples of 3 or 5

--- ex04.js
+++ ex04.js
@@ -14,3 +14,22 @@
 */
 
 // write your code below this comment
+
+function findSumByMultiples(nbr) {
+    var total = 0;
+    for (var i = 1; i < nbr; i++) {
+        total += IsMultipleOf3or5(i);
+    }
+    return total;
+}
+
+function IsMultipleOf3or5(nbr) {
+    var result = 0;
+    if (nbr % 3 == 0) result = nbr;
+    else if (nbr % 5 == 0) result = nbr;
+    return result;
+}
+
+console.log(findSumByMultiples(10));        // 23
+console.log(findSumByMultiples(16));        // 60
+console.log(findSumByMultiples(1000));       
