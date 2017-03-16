064411a07f94010eb69bc26a6a0b15a526f6a973
Amaurea <aurore.chatagnier@gmail.com>
2017-03-16 15:39:25 +0100

    converter number to price or price to number

--- ex03.js
+++ ex03.js
@@ -21,3 +21,21 @@
 */
 
 // write your code below this comment
+
+function numberToPrice(nbr) {
+    nbr = nbr.toFixed(2);           // affiche 2 chiffres significatifs
+    var price = nbr.toString().replace('.', ',') + '€';
+    if (nbr<0) price = '';
+    return price;
+}
+
+function priceToNumber(price) {
+    var nbr = parseFloat(price.replace(',', '.').replace('€', ''));
+    if (price == '') nbr = 0;
+    return nbr;
+}
+
+console.log(numberToPrice(4));
+console.log(priceToNumber("4,59€"));
+console.log(numberToPrice(-4));
+console.log(priceToNumber(''));
