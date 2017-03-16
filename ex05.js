c2bfc53fd346b8408600dc369bae7b1b0cb89ea6
Amaurea <aurore.chatagnier@gmail.com>
2017-03-16 17:05:36 +0100

    smallest positive number that is

--- ex05.js
+++ ex05.js
@@ -16,3 +16,35 @@
 
 // write your code below this comment
 
+
+function smallestMultipleFor(nbr) {
+    var array = createArray(nbr);
+    var smallestNumber = 1;
+
+    do {
+        var current_multiple = array[0]
+        smallestNumber = smallestNumber * current_multiple;
+        array = simplifyArrayMultiple(current_multiple, array);
+    } while (array.length != 0)
+
+    return smallestNumber;
+}
+
+function createArray(nbr) {
+    var array = [];
+    for (var i = 1; i <= nbr; i++) {
+        array[i - 1] = i;
+    }
+    return array;
+}
+
+function simplifyArrayMultiple(current_multiple, array) {       //same as decomposition in prime number
+    for (var i in array) {                                      // divide when number are divided by current_multiple, delete when number = 1
+        if (array[i] % current_multiple == 0) array[i] /= current_multiple;
+        if (array[i] == 1) array.splice(i, 1);
+    }
+    return array;
+}
+
+console.log(smallestMultipleFor(10));
+console.log(smallestMultipleFor(20));
